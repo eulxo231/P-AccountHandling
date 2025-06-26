faadda77439d3b6a088819d0ff2cbd855191754c
eulxo231 <redcheesesk@gmail.com>
2025-06-26 19:27:37 +0900

    m git commit -m

--- Bank Account System/app.py
+++ Bank Account System/app.py
@@ -1,25 +1,28 @@
-import Account
-import AccountDB
+db = AccountDB()
 
-from AccountDB import AccountDB
+# initial info
+account_number = '1234'
+account_password = '5678'
+user_name = '강서안'
+deposit = 1000000
 
-#TODO: Create menu, and print it infinitely
-# 1. Create 
-# 2. Deposit
-# 3. Withdraw
-# 4. Remittance
-# 5. Quit/Exit
+res = db.Create(account_number, account_password, user_name, deposit)
 
-db = AccountDB
+account_number = '1235'
+account_password = '1234'
+user_name = '홍길동'
+deposit = 1500000
+
+res = db.Create(account_number, account_password, user_name, deposit)
 
 while True:
-    print("===Bank Menu===")
+    print("=== Bank Menu ===")
     print(" 1. Create ")
     print(" 2. Deposit")
     print(" 3. Withdraw")
     print(" 4. Remittance")
     print(" 5. Quit/Exit")
-
+    print("=================")
     choice = int(input("Select an option: "))
     
     if choice == 1: 
@@ -28,58 +31,71 @@ while True:
         user_name = input("Enter user name: ")
         deposit = int(input("Enter initial deposit amount: "))
         res = db.Create(account_number, account_password, user_name, deposit)
-        print(res)
+        print(res['msg'])
 
     if choice == 2:
         account_number = input("Enter account number")
         res = db.FindIndexByAccountNumber(account_number)
-        if res['status'] == 'fail':
-            print("계좌 없음")
-        else:
+
+        if res['status'] == 'success':
             account_password = input("Enter password: ")
             account = db.account_list[res['index']]
             if account.account_password == account_password:
                 amt = int(input("Enter amount of deposit: "))
-                res = db.Update(account_number, amt)
-                print(res)
+                if amt >= 0:
+                  res = db.Update(account_number, amt)
+                  print("입금이 완료되었습니다")
+                else:
+                  print("입금액은 0보다 커야합니다")
             else:
                 print("비밀번호 불일치")
+        else:
+           print(res['msg'])
     
     if choice == 3:
         account_number = input("Enter account number")
         res = db.FindIndexByAccountNumber(account_number)
-        if res['status'] == 'fail':
-            print("계좌 없음")
-        else:
-            account_password = input("Enter password: ")
-            account = db.account_list[res['index']]
-            if account.account_password == account_password:
-                amt = int(input("Enter withdrawal amount: "))
+
+        if res['status'] == 'success':
+          account_password = input("Enter password: ")
+          if account_password == db.account_list[res['index']].account_password:
+              amt = int(input("Enter withdrawal amount: "))
+              if amt >= 0:
                 res = db.Update(account_number,-amt)
-                print(res)
-            else:
-                print("비밀번호 불일치")
+                if res['status'] == 'success':
+                  print("출금이 완료되었습니다")
+                else:
+                  print(res['msg'])
+              else:
+                print("출금액은 0보다 커야합니다")
+          else:
+              print("비밀번호 불일치")
+        else:
+          print("계좌 없음")
     
     if choice == 4:
         account_number = input("Enter account number")
         res = db.FindIndexByAccountNumber(account_number)
-        if res['status'] == 'fail':
-            print("계좌 없음")
+        
+        if res['status'] == 'success':
+          account_password = input("Enter password: ")
+          if account_password == db.account_list[res['index']].account_password:
+            rec_acc = input("Enter recipient account number: ")
+            res = db.FindIndexByAccountNumber(rec_acc)
+            if res['status'] == 'success':
+              amt = int(input("Enter amount to remit: "))
+              res = db.Update(account_number, -amt)
+              if res['status'] == 'success':
+               db.Update(rec_acc, amt)
+              else :
+                print(res['msg'])
+          else:
+            print("비밀번호 불일치")
         else:
-            account_password = input("Enter password: ")
-            account = db.account_list[res['index']]
-            if account.account_password == account_password:
-                rec_acc = input("Enter recipient account number: ")
-                amt = int(input("Enter amount to remit: "))
-                res = db.Update(account_number, -amt)
-                if res['status'] == 'success':
-                    res = db.Update(rec_acc, amt)
-                    print("Remittance successful.")
-                else:
-                    print("Insuffient amount of credits to remit.")
-            else:
-                print("비밀번호 불일치")
+          print(res['msg'])
+            
     
     if choice == 5:
         break
-        
+
+    db.Show()
